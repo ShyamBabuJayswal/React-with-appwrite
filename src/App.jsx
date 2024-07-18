c7fffa966e652f8a475c60730ccf761ea69bd21a
NIhal <jaisnj541@gmail.com>
2024-07-18 08:15:35 +0530

    Store config, component config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth";
+import {login,logout} from "./store/authSlice";
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading,setLoading] = useState(true);
 
-  return (
-    <>
-      
-      <h1>Blog App with appwrite</h1>   
-    </>
-  )
+  const dispatch = useDispatch();
+
+  useEffect(()  => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login(userData));
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false));
+  },[])
+
+
+  return !loading ?(
+    <div className='min-h-screen flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+    </div>
+  ): null
+    
+  
 }
 
 export default App
