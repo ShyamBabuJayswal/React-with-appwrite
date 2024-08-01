7f732372eb3463682dbf88c287ed1f9662ff5ade
NIhal <jaisnj541@gmail.com>
2024-08-01 14:57:25 +0530

    Router Part Complete

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,39 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux';
 import store from './store/store.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { AuthLayout, Login } from './components/index.js';
+import Home from './pages/Home.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[{
+        path:'/',
+        element: <Home />},
+        {
+          path:'/login',
+          element:(
+        <AuthLayout authentication ={false} >
+            <Login />
+          </AuthLayout>
+          )
+        }
+    ]
+
+    
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Provider store={store}>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
     </Provider>
   </React.StrictMode>,
 )
