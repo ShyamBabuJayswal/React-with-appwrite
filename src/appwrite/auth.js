7747cdf5703b1192e596e5f01f0df969078175d6
NIhal <jaisnj541@gmail.com>
2024-08-21 15:42:02 +0530

    Error solved

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,61 +1,43 @@
 import conf from '../conf/conf.js';
 import { Client, Account, ID } from "appwrite";
 
-
 export class AuthService {
     client = new Client();
     account;
 
     constructor() {
         this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId);
-        this.account = new Account(this.client);
-            
+            .setEndpoint(conf.appwriteUrl)  // Set Appwrite endpoint
+            .setProject(conf.appwriteProjectId);  // Set Appwrite project ID
+        this.account = new Account(this.client);  // Initialize the Account service
     }
 
-    async createAccount({email, password, name}) {
-        try {
-            const userAccount = await this.account.create(ID.unique(), email, password, name);
-            if (userAccount) {
-                // call another method
-                return this.login({email, password});
-            } else {
-               return  userAccount;
-            }
-        } catch (error) {
-            throw error;
+    // Create a new user account and log in
+    async createAccount({ email, password, name }) {
+        const userAccount = await this.account.create(ID.unique(), email, password, name);  // Create the user account
+        if (userAccount) {
+            return this.login({ email, password });  // Automatically log in after account creation
+        } else {
+            return userAccount;
         }
     }
 
-    async login({email, password}) {
-        try {
-            return await this.account.createEmailSession(email, password);
-        } catch (error) {
-            throw error;
-        }
+    // Log in with email and password
+    async login({ email, password }) {
+        return await this.account.createEmailPasswordSession(email, password);  // Create a session for the user
     }
 
+    // Get the currently logged-in user
     async getCurrentUser() {
-        try {
-            return await this.account.get();
-        } catch (error) {
-            console.log("Appwrite serive :: getCurrentUser :: error", error);
-        }
-
-        return null;
+        return await this.account.get();  // Fetch the current user
     }
 
+    // Log out the current user
     async logout() {
-
-        try {
-            await this.account.deleteSessions();
-        } catch (error) {
-            console.log("Appwrite serive :: logout :: error", error);
-        }
+        return await this.account.deleteSessions();  // Delete all sessions for the user
     }
 }
 
 const authService = new AuthService();
 
-export default authService
+export default authService;
